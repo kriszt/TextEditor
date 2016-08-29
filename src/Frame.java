42b11e7e75ded22568944ff167c3e09d4ace3e58
kriszt <kriszt_24@yahoo.com>
2016-08-29 18:09:05 +0300

    step by step

--- src/Frame.java
+++ src/Frame.java
@@ -1,36 +1,16 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Frame extends JFrame {
 
-    private JPanel panel = null;
-    private JMenuBar menuBar = null;
-    private JTextArea textArea = null;
+    private Panel panel = null;
 
     public Frame() {
         setTitle("Text Editor - Untitled");
         setSize(600, 400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        initComponents();
-
-        setVisible(true);
-
-    }
-
-    private void initComponents() {
-        panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-
-        textArea = new JTextArea();
-
-        menuBar = new JMenuBar();
-        //build menu
-
-        panel.add(menuBar, BorderLayout.NORTH);
-        panel.add(textArea, BorderLayout.CENTER);
-
+        panel = new Panel();
         add(panel);
+        setVisible(true);
     }
 
 }
