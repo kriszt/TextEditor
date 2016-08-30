83793e18198df0bd7e9cb4f7a545b069c7ff9a20
kriszt <kriszt_24@yahoo.com>
2016-08-30 19:49:02 +0300

    extended menu

--- src/MenuBar.java
+++ src/MenuBar.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 
 public class MenuBar extends JMenuBar {
 
-    private JMenu file = null;
-    private JMenuItem newFile = null, openFile = null, saveFile = null, saveFileAs = null;
+    private JMenu file, edit;
+    private JMenuItem newFile, openFile, saveFile, saveFileAs, copy, cut, paste, delete;
 
     public MenuBar() {
         file = new JMenu("File");
@@ -15,7 +15,19 @@ public class MenuBar extends JMenuBar {
         file.add(openFile);
         file.add(saveFile);
         file.add(saveFileAs);
+
+        edit = new JMenu("Edit");
+        copy = new JMenuItem("Copy");
+        cut = new JMenuItem("Cut");
+        paste = new JMenuItem("Paste");
+        delete = new JMenuItem("Delete");
+        edit.add(copy);
+        edit.add(cut);
+        edit.add(paste);
+        edit.add(delete);
+
         add(file);
+        add(edit);
     }
 
 }
